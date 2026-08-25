a870c51bf94960f9a5e5820219af31e57707ebd0
Mulpuri Yasaswy <ymulpuri@enphaseenergy.com>
2026-08-25 22:07:52 +0530

    Use PORT env variable for Railway

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const initSqlJs = require('sql.js');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const DB_PATH = path.join(__dirname, 'cadence.db');
 
 app.use(cors());
